90c3192b46bf115b240f03a2aea8604794783616
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-08-22 10:11:00 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,10 +1,3 @@
-arr = []
-target = input()
-
-for i in range(26):
-    try:
-        arr.append(target.index(chr(ord('a') + i)))
-    except ValueError:
-        arr.append(-1)
-
-print(*arr)
+s = input()
+for i in range(ord('a'), ord('z') + 1):
+    print(s.find(chr(i)), end = ' ')
