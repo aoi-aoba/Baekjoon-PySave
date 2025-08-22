5a4288192a8b7873e0a6567314f385fc1320c8f3
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-08-22 21:27:00 +0900

    [Silver V] Title: 너의 평점은, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.py
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -3,16 +3,13 @@ sumScore = sumGrade = 0
 
 for line in sys.stdin.readlines():
     name, score, grade = map(str, line.split())
-    if (grade == "P"): continue
 
-    if (grade.startswith("A")): gradeScore = 4.0
-    elif (grade.startswith("B")): gradeScore = 3.0
-    elif (grade.startswith("C")): gradeScore = 2.0
-    elif (grade.startswith("D")): gradeScore = 1.0
+    if grade == "P": continue
+    elif ord("E") - ord(grade[0]) >= 1: gradeScore = ord("E") - ord(grade[0])
     else: gradeScore = 0
 
-    if (grade.endswith("+")): gradeScore += 0.5
-    elif (grade.endswith("-")): gradeScore -= 0.5
+    if grade.endswith("+"): gradeScore += 0.5
+    elif grade.endswith("-"): gradeScore -= 0.5
 
     sumScore += float(score)
     sumGrade += float(score) * gradeScore
