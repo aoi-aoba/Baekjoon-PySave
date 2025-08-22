6b25d70b997ffe65c5349776a49807bed65d25b5
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-08-22 11:14:36 +0900

    [Bronze III] Title: 공 넣기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -0,0 +1,7 @@
+n, m = map(int, input().split())
+arr = [0 for _ in range(n)]
+for _ in range(m):
+    i, j, k = map(int, input().split())
+    for p in range(i - 1, j):
+        arr[p] = k
+print(*arr)
