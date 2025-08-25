14a8b850f51d17ef5fe4c64c4f0040869ca0257d
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-08-25 21:11:07 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 340 ms, Memory: 60360 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -1,11 +1,9 @@
+import sys
+
 n = int(input())
 points = []
-
 for _ in range(n):
-    x, y = map(int, input().split())
-    points.append((x, y))
-
-points.sort()
-
+    points.append(list(map(int, sys.stdin.readline().split())))
+points.sort(key=lambda point: (point[0], point[1]))
 for x, y in points:
     print(x, y)
