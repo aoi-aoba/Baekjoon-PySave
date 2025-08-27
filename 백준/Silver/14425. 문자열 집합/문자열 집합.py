245835868d79cd809042f249032488f20d6ca8c5
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-08-28 07:34:41 +0900

    [Silver IV] Title: 문자열 집합, Time: 88 ms, Memory: 37432 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -4,9 +4,4 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 wordSet = set(input().strip() for _ in range(n))
 
-result = 0
-for _ in range(m):
-    if input().strip() in wordSet:
-        result += 1
-
-print(result)
+print(sum(1 for _ in range(m) if input().strip() in wordSet))
