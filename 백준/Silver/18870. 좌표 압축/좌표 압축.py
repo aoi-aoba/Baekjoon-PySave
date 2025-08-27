24511e4cdc19413db8a33e1683aca9c0fdf0a33d
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-08-27 18:32:56 +0900

    [Silver II] Title: 좌표 압축, Time: 836 ms, Memory: 284056 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -10,5 +10,5 @@ sortArr = sorted(set(arr))
 # enumerate로 (인덱스, 값) 쌍 만들고 value:idx 형태로 매핑 dictionary 만듦
 rank = {value : idx for idx, value in enumerate(sortArr)}
 
-# arr 리스트 key 값인 x 값에 대하여 rank 접근
+# arr 리스트 key 값 x에 대하여 rank 접근
 print(" ".join(str(rank[x]) for x in arr))
