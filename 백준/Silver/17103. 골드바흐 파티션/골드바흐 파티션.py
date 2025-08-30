35bd6cdf12c5314fe583ea8ef91319838066d9d1
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-08-30 14:46:46 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 276 ms, Memory: 119308 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
@@ -2,6 +2,7 @@ MAX_VAL = 1_000_000
 
 def sieve(n):
     is_prime = [True] * (n + 1)
+    is_prime[0] = is_prime[1] = False
     end = int(n ** 0.5)
     for i in range(2, end + 1):
         if is_prime[i]:
@@ -12,9 +13,13 @@ def sieve(n):
 import sys
 input = sys.stdin.readline
 
+result = []
 primes = sieve(MAX_VAL)
 t = int(input())
 
 for _ in range(t):
     tar = int(input())
-    print(sum(1 for i in range(2, tar // 2 + 1) if primes[i] and primes[tar - i]))
+    cnt = sum(1 for i in range(2, tar // 2 + 1) if primes[i] and primes[tar - i])
+    result.append(str(cnt))
+
+print('\n'.join(result))
