62cad63ae866a25456197f446a700575e2dbb2e1
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-08-30 14:15:12 +0900

    [Silver II] Title: 베르트랑 공준, Time: 268 ms, Memory: 34340 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -1,20 +1,21 @@
 MAX_VAL = 123456 * 2
 
 def sieve(n):
-    arr = [i for i in range(n + 1)]
+    is_prime = [True] * (n + 1)
     end = int(n ** 0.5)
     for i in range(2, end + 1):
-        if arr[i] == 0:
-            continue
-        for j in range(i * i, n + 1, i):
-            arr[j] = 0
-    return set(i for i in arr[2:] if arr[i] != 0)
+        if is_prime[i]:
+            for j in range(i * i, n + 1, i):
+                is_prime[j] = False
+    return is_prime
 
 import sys
 input = sys.stdin.readline
 
 primes = sieve(MAX_VAL)
-num = int(input())
-while num != 0:
-    print(sum(1 for i in range(num + 1, 2 * num + 1) if i in primes))
+
+while True:
     num = int(input())
+    if num == 0:
+        break
+    print(sum(1 for i in range(num + 1, 2 * num + 1) if primes[i]))
