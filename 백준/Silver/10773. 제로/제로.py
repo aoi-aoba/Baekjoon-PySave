2553e8bb911c9145327ae4e696120bb4d81905b1
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-08-30 15:51:40 +0900

    [Silver IV] Title: 제로, Time: 64 ms, Memory: 33192 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,23 +1,13 @@
 import sys
 input = sys.stdin.readline
 
-class Stack:
-    def __init__(self):
-        self.items = []
-    def push(self, item):
-        self.items.append(item)
-    def pop(self):
-        return self.items.pop()
-    def to_list(self):
-        return self.items
-
-stack = Stack()
+stack = []
 K = int(input())
 for _ in range(K):
     temp = int(input())
     if temp == 0:
         stack.pop()
     else:
-        stack.push(temp)
+        stack.append(temp)
 
-print(sum(stack.to_list()))
+print(sum(stack))
