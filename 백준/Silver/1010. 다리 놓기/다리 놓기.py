9649aa6a572a6526b40139b23af27fa1072f2ff2
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-09-02 14:01:21 +0900

    [Silver V] Title: 다리 놓기, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,5 +1,18 @@
-import math
+def fact(n):
+    if n <= 1:
+        return 1
+    res = 1
+    for i in range(1, n + 1):
+        res *= i
+    return res
+
+
+def comb(n, r):
+    if n < r:
+        temp = n; n = r; r = temp
+    return fact(n) // (fact(n - r) * fact(r))
+
 
 for i in range(int(input())):
-    n, m = map(int, input().split())
-    print(math.comb(n, m) if n > m else math.comb(m, n))
+    N, M = map(int, input().split())
+    print(comb(N, M))
