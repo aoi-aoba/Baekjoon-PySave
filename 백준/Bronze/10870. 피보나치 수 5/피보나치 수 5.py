1ee52280f112e12c7af0b9823242ef31ff3f15be
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-09-04 09:15:59 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,9 +1,10 @@
 def fibo(n):
-    if n == 0:
-        return 0
-    elif n <= 2:
-        return 1
-    return fibo(n-1) + fibo(n-2)
+    a = b = 1
+    if n <= 1:
+        return n
+    for i in range(1, n):
+        a, b = b, b + a
+    return a
 
 import sys
 print(fibo(int(sys.stdin.readline())))
