7084e868cd3f3647b77e7f878b2101fe404c576b
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-09-04 09:24:34 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 396 ms, Memory: 38672 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.py
@@ -0,0 +1,21 @@
+from pkgutil import get_loader
+
+
+def recursion(s, l, r):
+    global cnt
+    cnt += 1
+
+    if l >= r: return 1
+    elif s[l] != s[r]: return 0
+    else: return recursion(s, l + 1, r - 1)
+
+def isPalindrome(s):
+    return recursion(s, 0, len(s) - 1)
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+for _ in range(n):
+    cnt = 0
+    print(isPalindrome(input().rstrip()), cnt)
