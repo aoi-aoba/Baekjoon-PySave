c345b5ec4dc99ae080e7c9e2391626cbd425d6ce
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-09-04 15:42:56 +0900

    [Silver III] Title: N과 M (4), Time: 44 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+def tracker(n):
+    if len(res) == M:
+        print(' '.join(map(str, res)))
+        return
+
+    for i in range(n, N+1):
+        res.append(i)
+        tracker(i)
+        res.pop()
+
+N, M = map(int, input().split())
+res = []
+
+tracker(1)
