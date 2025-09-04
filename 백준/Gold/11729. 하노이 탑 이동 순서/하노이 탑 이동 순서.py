1c9c71d898aa1a5b630c6a6aa3a656b75fbc651a
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-09-04 15:15:47 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 248 ms, Memory: 219916 KB -BaekjoonHub

--- 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.py
@@ -1,12 +1,15 @@
+import sys
+
 def hanoi(n, start, via, dest):
     if n == 1:
-        print(start, dest)
+        moves.append(str(start) + " " + str(dest))
     else:
         hanoi(n - 1, start, dest, via)
-        print(start, dest)
+        moves.append(str(start) + " " + str(dest))
         hanoi(n - 1, via, start, dest)
 
-import sys
 n = int(sys.stdin.readline())
 print(2 ** n - 1)
+moves = []
 hanoi(n, 1, 2, 3)
+sys.stdout.write("\n".join(moves))
